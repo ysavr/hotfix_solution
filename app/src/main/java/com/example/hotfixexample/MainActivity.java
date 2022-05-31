ba42c3944f4bcad8b117ae41610c0418b2317a4e
yusuf <yusuf@tlab.co.id>
2022-05-31 10:40:51 +0700

    init

---
+++ app/src/main/java/com/example/hotfixexample/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.hotfixexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
+
+import com.tencent.tinker.lib.tinker.TinkerInstaller;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        askForRequiredPermissions();
+
+        Button loadPatchButton = (Button) findViewById(R.id.loadPatch);
+
+        loadPatchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getPatch();
+            }
+        });
+    }
+
+    private void getPatch() {
+        if (BuildConfig.DEBUG) {
+            TinkerInstaller.onReceiveUpgradePatch(getApplicationContext(),
+                    Environment.getExternalStorageDirectory().getAbsolutePath()
+                            + "/app-debug-patch_signed_7zip.apk");
+        } else {
+            TinkerInstaller.onReceiveUpgradePatch(getApplicationContext(),
+                    Environment.getExternalStorageDirectory().getAbsolutePath()
+                            + "/app-release-patch_signed_7zip.apk");
+        }
+    }
+
+    private void askForRequiredPermissions() {
+        if (Build.VERSION.SDK_INT < 23) {
+            return;
+        }
+        if (!hasRequiredPermissions()) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, 0);
+        }
+    }
+
+    private boolean hasRequiredPermissions() {
+        if (Build.VERSION.SDK_INT >= 16) {
+            final int res = ContextCompat.checkSelfPermission(this.getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE);
+            return res == PackageManager.PERMISSION_GRANTED;
+        } else {
+            // When SDK_INT is below 16, READ_EXTERNAL_STORAGE will also be granted if WRITE_EXTERNAL_STORAGE is granted.
+            final int res = ContextCompat.checkSelfPermission(this.getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            return res == PackageManager.PERMISSION_GRANTED;
+        }
+    }
+}
